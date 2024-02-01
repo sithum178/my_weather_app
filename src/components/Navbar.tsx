a90533eeb0cf1bab67b248b8ad8c2cd858688782
sithum <sithumr135@gmail.com>
2024-02-01 19:37:20 +0530

    second commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -87,38 +87,57 @@ const Navbar = ({ location }: Props) => {
   }
 
   return (
-    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-      <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
-        <div className="flex items-center justify-center gap-2">
-          <h2 className="text-gray-500 text-3xl">Weather</h2>
-          <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
-        </div>
-        <section className="flex gap-2 items-center ">
-          <MdMyLocation
-            title="your current location"
-            onClick={handleCurrentLocation}
-            className="text-2xl text-gray-400 hover:text-gray-600 cursor-pointer"
-          />
-          <MdOutlineLocationOn className="text-3xl cursor-pointer hover:text-blue-800" />
-          <p className="text-slate-900/80 text-sm">{location}</p>
-          <div className="relative">
-            <SearchBox
-              value={city}
-              onSubmit={handleSubmitSearch}
-              onChange={(e) => handleInputChange(e.target.value)}
-            />
-            <SuggestionBox
-              {...{
-                showSuggestions,
-                suggestions,
-                handleSuggestionClick,
-                error,
-              }}
-            />
+    <>
+      <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+        <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+          <div className="flex items-center justify-center gap-2">
+            <h2 className="text-gray-500 text-3xl">Weather</h2>
+            <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
           </div>
-        </section>
-      </div>
-    </nav>
+          <section className="flex gap-2 items-center ">
+            <MdMyLocation
+              title="your current location"
+              onClick={handleCurrentLocation}
+              className="text-2xl text-gray-400 hover:text-gray-600 cursor-pointer"
+            />
+            <MdOutlineLocationOn className="text-3xl cursor-pointer hover:text-blue-800" />
+            <p className="text-slate-900/80 text-sm">{location}</p>
+            <div className="relative hidden md:flex">
+              <SearchBox
+                value={city}
+                onSubmit={handleSubmitSearch}
+                onChange={(e) => handleInputChange(e.target.value)}
+              />
+              <SuggestionBox
+                {...{
+                  showSuggestions,
+                  suggestions,
+                  handleSuggestionClick,
+                  error,
+                }}
+              />
+            </div>
+          </section>
+        </div>
+      </nav>
+      <section className="flex max-w-7xl px-3 justify-center items-center  md:hidden">
+        <div className="relative ">
+          <SearchBox
+            value={city}
+            onSubmit={handleSubmitSearch}
+            onChange={(e) => handleInputChange(e.target.value)}
+          />
+          <SuggestionBox
+            {...{
+              showSuggestions,
+              suggestions,
+              handleSuggestionClick,
+              error,
+            }}
+          />
+        </div>
+      </section>
+    </>
   );
 };
 
